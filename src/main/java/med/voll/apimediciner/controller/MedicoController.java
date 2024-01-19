7207b51b1e804963d208fc3067b276d1b82f6f62
CarlosBotelho42 <carlospiece55@gmail.com>
2024-01-18 22:18:15 -0300

    feat: criação dos repositories e construtores para a conversao de dto para entidade

--- src/main/java/med/voll/apimediciner/controller/MedicoController.java
+++ src/main/java/med/voll/apimediciner/controller/MedicoController.java
@@ -1,6 +1,8 @@
 package med.voll.apimediciner.controller;
 
 import med.voll.apimediciner.medico.DadosCadastroMedico;
+import med.voll.apimediciner.medico.Medico;
+import med.voll.apimediciner.medico.MedicoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    private final MedicoRepository repository;
+
+    public MedicoController(MedicoRepository repository) {
+        this.repository = repository;
+    }
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
---
+++ src/main/java/med/voll/apimediciner/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package med.voll.apimediciner.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+}
