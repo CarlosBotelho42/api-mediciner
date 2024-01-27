7a03f84b259c0fb5c46b9bd9b193e926fb52cc87
CarlosBotelho42 <carlospiece55@gmail.com>
2024-01-27 15:20:54 -0300

    feat: implementação do Get para a listagem de medicos(utilizando DTO para trazer apenas os dados que preciso)

--- src/main/java/med/voll/apimediciner/controller/MedicoController.java
+++ src/main/java/med/voll/apimediciner/controller/MedicoController.java
@@ -3,12 +3,12 @@ package med.voll.apimediciner.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.apimediciner.medico.DadosCadastroMedico;
+import med.voll.apimediciner.medico.DadosListagemMedico;
 import med.voll.apimediciner.medico.Medico;
 import med.voll.apimediciner.medico.MedicoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -24,6 +24,13 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+
     }
 
 }
---
+++ src/main/java/med/voll/apimediciner/medico/DadosListagemMedico.java
@@ -0,0 +1,16 @@
+package med.voll.apimediciner.medico;
+
+public record DadosListagemMedico(
+
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade
+
+) {
+
+    public DadosListagemMedico(Medico medico) {
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+
+}
