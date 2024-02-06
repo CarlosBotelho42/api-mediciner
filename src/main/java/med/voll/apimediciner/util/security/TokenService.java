747d41ce9c5e537d29bc7c845c72fa80edf9f890
CarlosBotelho42 <carlospiece55@gmail.com>
2024-02-06 19:19:11 -0300

    feat: criando um getSubject para o token ser validado e tambem trazer o usuario que esta fazendo o uso

--- src/main/java/med/voll/apimediciner/util/security/TokenService.java
+++ src/main/java/med/voll/apimediciner/util/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.apimediciner.util.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.apimediciner.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,20 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar o token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Mediciner")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT invalido ou expirado");
         }
     }
 
