5bbe7eb7b8fd96c1b5d0360c052ce61651f5a969
CarlosBotelho42 <carlospiece55@gmail.com>
2024-02-06 19:01:08 -0300

    feat: travando reqs sem o token passado para o header

--- src/main/java/med/voll/apimediciner/util/security/SecurityFilter.java
+++ src/main/java/med/voll/apimediciner/util/security/SecurityFilter.java
@@ -15,7 +15,22 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        var tokenJWT = recuperarToken(request);
+
+        System.out.println(tokenJWT);
+
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null){
+            throw  new RuntimeException("Toke JWT não enviado o Header Authorization");
+        }
+
+        return authorizationHeader.replace("Bearer ", "");
 
     }
 }
