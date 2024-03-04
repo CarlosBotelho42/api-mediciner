1bd6be3b299183f254275a59dd7f2c332efbdecf
CarlosBotelho42 <carlospiece55@gmail.com>
2024-03-03 22:03:09 -0300

    feat: Validacao para o horario de funcionamento

---
+++ src/main/java/med/voll/apimediciner/domain/consulta/validador/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,20 @@
+package med.voll.apimediciner.domain.consulta.validador;
+
+import med.voll.apimediciner.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.apimediciner.util.exception.ValidationConsultaException;
+
+import java.time.Duration;
+
+public class ValidadorHorarioAntecedencia {
+
+    public void validar(DadosAgendamentoConsulta dados){
+
+        var dadosConsulta = dados.data();
+        var agora = dadosConsulta.toLocalTime();
+        var diferencaEmMinutos = Duration.between(agora, dadosConsulta).toMinutes();
+
+        if(diferencaEmMinutos < 30){
+            throw new ValidationConsultaException("Horario menor do que o minimo para marcar um consulta!");
+        }
+    }
+}
