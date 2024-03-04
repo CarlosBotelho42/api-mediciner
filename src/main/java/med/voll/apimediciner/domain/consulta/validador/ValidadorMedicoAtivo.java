53b812c44dd94106b803aeed2ebcc193ba45fce5
CarlosBotelho42 <carlospiece55@gmail.com>
2024-03-03 22:03:44 -0300

    feat: Validacao para medico ativo

---
+++ src/main/java/med/voll/apimediciner/domain/consulta/validador/ValidadorMedicoAtivo.java
@@ -0,0 +1,22 @@
+package med.voll.apimediciner.domain.consulta.validador;
+
+import med.voll.apimediciner.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.apimediciner.domain.medico.MedicoRepository;
+import med.voll.apimediciner.util.exception.ValidationConsultaException;
+
+public class ValidadorMedicoAtivo {
+
+    private MedicoRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+
+        if (dados.idMedico() == null){
+            return;
+        }
+
+        var medicoAtivo = repository.findAtivoById(dados.idMedico());
+        if(!medicoAtivo){
+            throw new ValidationConsultaException("Medico escolhico nao existe!");
+        }
+    }
+}
