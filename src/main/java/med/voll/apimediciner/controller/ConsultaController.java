f8bb37336f79d9014a52e156bc536ecabecabf50
CarlosBotelho42 <carlospiece55@gmail.com>
2024-03-09 14:28:11 -0300

    feat: adicionanto token para a interface do springDoc

--- src/main/java/med/voll/apimediciner/controller/ConsultaController.java
+++ src/main/java/med/voll/apimediciner/controller/ConsultaController.java
@@ -1,5 +1,6 @@
 package med.voll.apimediciner.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.apimediciner.domain.consulta.DadosAgendamentoConsulta;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
--- src/main/java/med/voll/apimediciner/controller/PacienteController.java
+++ src/main/java/med/voll/apimediciner/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.apimediciner.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.apimediciner.domain.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
 
     @Autowired
