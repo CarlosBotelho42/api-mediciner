0c2bf333eb343e67e57afcf666c55f7772cb7a9c
CarlosBotelho42 <carlospiece55@gmail.com>
2024-03-09 14:37:12 -0300

    feat: Customizando Header da documentação

--- src/main/java/med/voll/apimediciner/util/springDocs/SpringDocsConfigurations.java
+++ src/main/java/med/voll/apimediciner/util/springDocs/SpringDocsConfigurations.java
@@ -2,6 +2,9 @@ package med.voll.apimediciner.util.springDocs;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,14 @@ public class SpringDocsConfigurations {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                .title("Mediciner API")
+                .description("API Rest da aplicação api.mediciner, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
+                .contact(new Contact()
+                        .name("Carlao")
+                        .email("carlospiece55@gmail.com"))
+                .termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
