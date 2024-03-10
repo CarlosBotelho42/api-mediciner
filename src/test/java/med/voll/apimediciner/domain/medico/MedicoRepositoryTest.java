abb21b8bff18b017dc9be56c7941808e64326ec6
CarlosBotelho42 <carlospiece55@gmail.com>
2024-03-10 14:49:28 -0300

    feat: Criancao dos testes para as querys do medico

---
+++ src/test/java/med/voll/apimediciner/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,20 @@
+package med.voll.apimediciner.domain.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioDataLivre(){
+
+    }
+
+}
